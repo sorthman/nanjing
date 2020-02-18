097824a1dcebf2958639d2bbeb28504de7039abc
zoujp <zoujp@192.168.1.107>
2020-02-18 14:55:55 +0800

    bug修改

--- pzmall-db/src/main/java/org/linlinjava/litemall/db/dao/ExWhUserMapper.java
+++ pzmall-db/src/main/java/org/linlinjava/litemall/db/dao/ExWhUserMapper.java
@@ -1,11 +1,12 @@
 package org.linlinjava.litemall.db.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.linlinjava.litemall.db.domain.Whuser;
 
 public interface ExWhUserMapper {
 
-	List<Whuser> getUserListBySearch(List<String> usertypes);
+	List<Whuser> getUserListBySearch(Map<String, Object> usertypes);
 
 }
